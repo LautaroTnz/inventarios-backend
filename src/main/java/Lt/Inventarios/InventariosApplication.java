ae4abc96c66b59d1b543be066ca9316b98777659
Lautaro Tintez <lautarotintez@gmail.com>
2024-04-11 11:48:51 -0300

    ajustes en CORS

--- src/main/java/Lt/Inventarios/InventariosApplication.java
+++ src/main/java/Lt/Inventarios/InventariosApplication.java
@@ -2,8 +2,11 @@ package Lt.Inventarios;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
+@CrossOrigin(origins = "https://inventarios-frontend.vercel.app")
+
 public class InventariosApplication {
 
 	public static void main(String[] args) {
