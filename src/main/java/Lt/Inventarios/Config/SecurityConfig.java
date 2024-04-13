6b7734fd0860f06fc9fa668d1d5bb08d48990744
Lautaro Tintez <lautarotintez@gmail.com>
2024-04-13 17:42:44 -0300

    ajustes metodos seguridad

--- src/main/java/Lt/Inventarios/Config/SecurityConfig.java
+++ src/main/java/Lt/Inventarios/Config/SecurityConfig.java
@@ -29,6 +29,7 @@ public class SecurityConfig {
                             authRequest
                                     .requestMatchers(HttpMethod.GET).permitAll()
                                     .requestMatchers(HttpMethod.DELETE).permitAll()
+                                    .requestMatchers(HttpMethod.POST).permitAll()
                                     .requestMatchers(HttpMethod.PUT).permitAll()
                                     .requestMatchers(HttpMethod.OPTIONS).permitAll()
                                     .requestMatchers("/auth/**").permitAll()
